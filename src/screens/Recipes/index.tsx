6be0ad753b7e2c769b6d5be3c1001c51c4b79422
Hao Nguyen <vuhao7920@gmail.com>
2023-07-18 17:35:46 +0700

    update

--- src/screens/Recipes/index.tsx
+++ src/screens/Recipes/index.tsx
@@ -1,34 +1,39 @@
-import React, { useState } from "react";
-import styles from "./style.module.css";
-import { Link, Outlet } from "react-router-dom";
+// import React, { useState } from "react";
+// import styles from "./style.module.css";
+// import { Link, Outlet } from "react-router-dom";
+// const dataT = [
+//   {
+//     name: "hamburger",
+//     description: "mon ngon",
+//   },
+//   {
+//     name: "banh my",
+//     description: "my ngon",
+//   },
+// ];
+// interface Recipe {
+//   name: string;
+//   description: string;
+// }
+// function Recipes() {
+//   const [recipes] = useState(dataT);
+//   return (
+//     <div className={styles.container}>
+//       <div className={styles.leftSide}>
+//         <Link to="form" className="btn btn-success mb-3">
+//           New Recipe
+//         </Link>
+//         <div id="content">
+//           {recipes.map((r)=>{
 
-interface Props {
-  name: string;
-  description: string;
-}
-const Recipes = () => {
-  const [recipes] = useState<Props[]>([
-    {
-      name: "hamburger",
-      description: "mon ngon",
-    },
-    {
-      name: "banh my",
-      description: "my ngon",
-    },
-  ]);
-  return (
-    <div className={styles.container}>
-      <div className={styles.leftSide}>
-        <Link to="form" className="btn btn-success mb-3">
-          New Recipe
-        </Link>
-      </div>
-      <div className={styles.rightSide}>
-        <Outlet />
-      </div>
-    </div>
-  );
-};
+//           })}
+//         </div>
+//       </div>
+//       <div className={styles.rightSide}>
+//         <Outlet />
+//       </div>
+//     </div>
+//   );
+// }
 
-export default Recipes;
+// export default Recipes;
