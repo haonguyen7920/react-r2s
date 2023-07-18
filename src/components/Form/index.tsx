2adbdfebeb1d85773f1b524677d6d0a2753e0d0d
Hao Nguyen <vuhao7920@gmail.com>
2023-07-18 13:17:37 +0700

    update

--- src/components/Form/index.tsx
+++ src/components/Form/index.tsx
@@ -1,23 +1,27 @@
 import React from "react";
-import { Form, redirect } from "react-router-dom";
+import { Form } from "react-router-dom";
 
 const MyForm = () => {
   return (
-    <Form method="post" action="/recipes/form">
+    <Form method="post" action="/recipes/form ">
+      <button className="btn btn-success" type="submit">
+        Save
+      </button>
       <input type="email" name="email" required />
       <textarea name="message" required />
-      <button>Submit</button>
     </Form>
   );
 };
 export const contactAction = async ({ request }: any) => {
-  const data = await request.formData();
-  //   const submission = {
-  //     email: data.get("email"),
-  //     message: data.get("message"),
-  //   };
-  console.log(data);
-  return redirect("/");
+  const formData = await request.formData();
+  const data = Object.fromEntries(formData);
+
+  // const submission = {
+  //   email: formData.get("email"),
+  //   message: formData.get("message"),
+  // };
+  // console.log(submission);
+  return { data };
 };
 
 export default MyForm;
--- src/screens/Recipes/index.tsx
+++ src/screens/Recipes/index.tsx
@@ -1,18 +1,20 @@
-import React from 'react'
-import styles from './style.module.css'
-import { Link, Outlet } from 'react-router-dom'
+import React from "react";
+import styles from "./style.module.css";
+import { Link, Outlet } from "react-router-dom";
 
 const Recipes = () => {
   return (
-      <div className={styles.container}>
-        <div className={styles.leftSide}>
-          <Link to="form" className='btn btn-success mb-3'>New Recipe</Link>
-        </div>
-        <div className={styles.rightSide}>
-          <Outlet/>
-        </div>
+    <div className={styles.container}>
+      <div className={styles.leftSide}>
+        <Link to="form" className="btn btn-success mb-3">
+          New Recipe
+        </Link>
       </div>
-  )
-}
+      <div className={styles.rightSide}>
+        <Outlet />
+      </div>
+    </div>
+  );
+};
 
-export default Recipes
+export default Recipes;
