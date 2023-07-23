f6a40abd1d6faed3dd770c381b87766129af4034
Hao Nguyen <vuhao7920@gmail.com>
2023-07-23 19:57:11 +0700

    update

--- src/components/Form/index.tsx
+++ src/components/Form/index.tsx
@@ -1,22 +1,57 @@
-import React from "react";
-import { useNavigate } from "react-router-dom";
+import React, { useState } from "react";
+import { Formik, Form, Field } from "formik";
+import * as Yup from "yup";
+import styles from "./style.module.css";
+
+const SignupSchema = Yup.object().shape({
+  name: Yup.string().required("Required"),
+  image: Yup.string().required("Required"),
+  description: Yup.string().required("Required"),
+});
 
 const MyForm = () => {
-  const navigate = useNavigate();
-  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-    navigate("/recipes", {
-      state: { name: "banh Hamburger", description: "green" },
-    });
-  };
+  // const [imageUrl, setImageUrl] = useState("");
+  // const handleInputChange = (e: any) => {
+  //   setImageUrl(e.target.value);
+  // };
   return (
-    <form onSubmit={handleSubmit}>
-      <button className="btn btn-success" type="submit">
-        Save
-      </button>
-      <input type="email" name="email" required />
-      <textarea name="message" required />
-    </form>
+    <div>
+      <Formik
+        initialValues={{
+          name: "",
+          image: "",
+          description: "",
+        }}
+        validationSchema={SignupSchema}
+        onSubmit={(values) => {
+          // same shape as initial values
+          console.log(values);
+        }}
+      >
+        {({ errors, touched, values }) => (
+          <Form className={styles.container}>
+            <div>
+              <button type="submit" className="btn btn-success">
+                Save
+              </button>
+              <button className="btn btn-danger">Cancel</button>
+            </div>
+            <label htmlFor="">Name</label>
+            <Field name="name" className="form-control" />
+            {errors.name && touched.name && <small>{errors.name}</small>}
+            <label htmlFor="">Image URL</label>
+            <Field name="image" className="form-control" />
+            {errors.image && touched.image && <small>{errors.image}</small>}
+            {values.image && <img src={values.image} alt="Preview" />}
+            <label htmlFor="">Description</label>
+            <Field name="description" type="text" className="form-control" />
+            {errors.description && touched.description && (
+              <small>{errors.description}</small>
+            )}
+          </Form>
+        )}
+      </Formik>
+    </div>
   );
 };
 
